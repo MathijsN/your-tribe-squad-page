b24d0dfded59e96d8fd147957d619f09d3cd7737
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-09-08 21:22:02 +0200

    Seperated data set

---
+++ scripts/data.js
@@ -0,0 +1,17 @@
+export const arr  = [
+    {
+        name: "Mathijs",
+        img: "https://townsquare.media/site/295/files/2020/09/super-mario-bros.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+        profileCard: "https://mathijsn.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Yassine",
+        img: "https://unocasa.com/cdn/shop/articles/types_of_coffee_91a828a5-7ff3-427d-acaa-c8b7289c9e5a_600x.jpg?v=1621261041",
+        profileCard: "https://statics.teams.cdn.office.net/evergreen-assets/safelinks/2/atp-safelinks.html"
+        },
+    {
+        name: "Anass",
+        img: "https://img.freepik.com/free-photo/anime-eyes-illustration_23-2151660487.jpg",
+        profileCard: "https://anass-kh.github.io/your-tribe-profile-card/"
+        }
+]
--- scripts/mathijs.js
+++ scripts/mathijs.js
@@ -1,22 +1,4 @@
-const arr = [
-    {
-        name: "Mathijs",
-        img: "https://townsquare.media/site/295/files/2020/09/super-mario-bros.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
-        profileCard: "https://mathijsn.github.io/your-tribe-profile-card/"
-        },
-    {
-        name: "Yassine",
-        img: "https://unocasa.com/cdn/shop/articles/types_of_coffee_91a828a5-7ff3-427d-acaa-c8b7289c9e5a_600x.jpg?v=1621261041",
-        profileCard: "https://statics.teams.cdn.office.net/evergreen-assets/safelinks/2/atp-safelinks.html"
-        },
-    {
-        name: "Anass",
-        img: "https://img.freepik.com/free-photo/anime-eyes-illustration_23-2151660487.jpg",
-        profileCard: "https://anass-kh.github.io/your-tribe-profile-card/"
-        }
-]
-
-const randomNumberArr = []
+import { arr } from '../scripts/data.js'
 
 const gridElement = document.querySelector(".bento-grid")
 
@@ -38,14 +20,4 @@ function renderTable() {
 )
 }
 
-
-function randomNumber() {
-    const number = Math.floor(Math.random() * arr.length).toString()
-
-    randomNumber.push(number)
-}
-
 renderTable()
-randomNumber()
-
-console.log(randomNumber)
