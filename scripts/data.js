d3e095f9e508f825f536043689d9c9e038a506de
MathijsN <72303334+MathijsN@users.noreply.github.com>
2025-09-10 12:17:39 +0200

    Added data

--- scripts/data.js
+++ scripts/data.js
@@ -1,28 +1,138 @@
 export const arr  = [
     {
         name: "Mathijs",
-        img: "https://townsquare.media/site/295/files/2020/09/super-mario-bros.jpg?w=1200&h=0&zc=1&s=0&a=t&q=89",
+        img: "https://vitalgym.nl/wp-content/uploads/2022/05/DSC04854-1024x684-1.jpg",
         profileCard: "https://mathijsn.github.io/your-tribe-profile-card/"
         },
     {
-        name: "Yassine",
-        img: "https://unocasa.com/cdn/shop/articles/types_of_coffee_91a828a5-7ff3-427d-acaa-c8b7289c9e5a_600x.jpg?v=1621261041",
-        profileCard: "https://statics.teams.cdn.office.net/evergreen-assets/safelinks/2/atp-safelinks.html"
+        name: "MLH",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/muller-lulofshuis-hva-amsterdam.jpeg/muller-lulofshuis-hva-amsterdam.jpeg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://www.hva.nl/locaties/muller-lulofshuis",
+        class: "span2"
         },
     {
         name: "Yassine",
-        img: "https://unocasa.com/cdn/shop/articles/types_of_coffee_91a828a5-7ff3-427d-acaa-c8b7289c9e5a_600x.jpg?v=1621261041",
-        profileCard: "https://statics.teams.cdn.office.net/evergreen-assets/safelinks/2/atp-safelinks.html"
+        img: "https://www.suzhouprivatetour.com/upload/ueditor/image/20170803/6363736321759873439459278.jpg",
+        profileCard: "https://yassineak1.github.io/your-tribe-profile-card/"
         },
     {
         name: "Anass",
         img: "https://img.freepik.com/free-photo/anime-eyes-illustration_23-2151660487.jpg",
-        profileCard: "https://anass-kh.github.io/your-tribe-profile-card/",
+        profileCard: "https://anass-kh.github.io/your-tribe-profile-card/"
         },
     {
-        name: "BPH",
+        name: "Maarten",
+        img: "https://kvadrat.az/uploads/articles/666205d642ae1.jpg",
+        profileCard: "https://maartentepoele.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Sadaf",
+        img: "https://defendersofwildlife-360365372.imgix.net/sites/default/files/2019-04/Ocean%20Pixabay%20header.jpg?fit=max&ixlib=php-4.1.0&w=1110",
+        profileCard: "https://sadaf-design.github.io/your-tribe-profile-card/"
+        },
+    {
+            name: "Sieuwke",
+            img: "https://moowy.nl/wp-content/uploads/sites/3/2023/11/wilde-bloemen.jpeg",
+            profileCard: "https://sieuwkesheta.github.io/your-tribe-profile-card/"
+        },
+    {
+         name: "BPH",
+         img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+         profileCard: "https://www.hva.nl/locaties/benno-premselahuis",
+         class: "span2"
+         },
+    {
+        name: "Luka",
         img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
-        profileCard: "https://www.hva.nl/locaties/benno-premselahuis",
+        profileCard: "https://millie8969.github.io/lmd-profile-card/"
+        },
+    {
+        name: "Bronx",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://bronxpostma13.github.io/your-tribe-profile-card/"
+        },
+     {
+        name: "TTH",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/theo-thijssenhuis-hva-amsterdam.jpeg/theo-thijssenhuis-hva-amsterdam.jpeg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://www.hva.nl/locaties/theo-thijssenhuis",
         class: "span2"
-        }
+        },
+    {
+        name: "Jamie",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://jamievos991.github.io/HVA-Visitekaartje/"
+        },
+    {
+        name: "Chama",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://chewsy.github.io/visitekaartje/"
+        },
+    {
+        name: "Kurollos",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://kurollos.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Ke",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://kebiemen.github.io/your-tribe-profile-card/"
+        },
+        {
+        name: "KSH",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/kohnstammhuis-hva-amsterdam.jpeg/kohnstammhuis-hva-amsterdam.jpeg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://www.hva.nl/locaties/kohnstammhuis",
+        class: "span2"
+        },
+    {
+        name: "Kevin",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://toasterbath0758.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "JMH",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/jmh-amstelcampus.jpg/jmh-amstelcampus.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://www.hva.nl/locaties/jakoba-mulderhuis",
+        class: "span2"
+        },
+    {
+        name: "Ahlam",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://addaahva.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Mete",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://mete2004.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Jasin",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://jasinahmed.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Tzgaming",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://tzgaming.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "KMH",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/koetsier-montaignehuis-hva-amsterdam.jpeg/koetsier-montaignehuis-hva-amsterdam.jpeg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://www.hva.nl/locaties/koetsier-montaignehuis",
+        class: "span2"
+        },
+    {
+        name: "Lynn",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://lynnvdbo.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Julian",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://juliandavelaar.github.io/your-tribe-profile-card/"
+        },
+    {
+        name: "Ayoub",
+        img: "https://cms-prd.cms.hva.nl/site/binaries/content/gallery/shared/locatie/portrait-images/hva-locaties/benno-premselahuis-bph.jpg/benno-premselahuis-bph.jpg/hvacontentservice%3AoneByOneLarge",
+        profileCard: "https://ayoubkh1.github.io/your-tribe-profile-card/"
+        },
 ]
